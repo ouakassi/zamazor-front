945a99866d8ff4027937b22a8a3125ba2078efec
oussama ouakassi <lmoutchoo@gmail.com>
2026-06-28 12:38:19 +0100

    Initialize app auth state as guest if no token is saved and restrict session expiration redirects to non-public routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import { Toaster } from "sonner";
 import { RouterProvider } from "react-router";
 import { useEffect } from "react";
 import { userService } from "./features/auth/services/usersService";
+import { tokenManager } from "./features/auth/globals/tokenManager";
+import { useAuthStore } from "./features/auth/stores/authStore";
+import { AuthStatus } from "./features/auth/types";
 
 function App() {
 	useEffect(() => {
-		userService.fetchCurrentUser();
+		const token = tokenManager.getAccessToken();
+		if (token) {
+			userService.fetchCurrentUser();
+		} else {
+			useAuthStore.setState({ status: AuthStatus.Unauthenticated });
+		}
 	}, []);
 
 	return (
